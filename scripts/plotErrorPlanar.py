d1603d5b6e8c29303530d9bba89100bb70e26594
Jakub Ocwieja <netkuba@students.mimuw.edu.pl>
2014-04-17 20:52:51 +0200

    plot

--- scripts/plotErrorPlanar.py
+++ scripts/plotErrorPlanar.py
@@ -9,8 +9,12 @@ fig, ax1 = plt.subplots()
 ax2 = ax1.twinx();
 ax = [ax1, ax2];
 
-ax1.set_xlabel('logarithm of ratio of update queries to distance queries')
-ax1.set_ylabel('time (sec)')
+ax1.set_xlabel('approximation parameter')
+ax1.set_ylabel('time (sec)', color='r')
+ax1.set_ylim([0,4])
+ax2.set_ylabel('approximation ratio', color='b')
+#ax2.set_yscale('log')
+ax2.set_ylim([1,1.04])
 
 symbols = ['-ro', '-.bs']
 colors = ['r', 'b']
@@ -21,7 +25,7 @@ a = [line.split() for line in sys.stdin]
 a = [list(x) for x in zip(*a)];
 
 labels = a[0];
-k = (len(labels)-1)/4;
+k = (len(labels)-1)/6;
 print(labels);
 for i in range(0, len(labels)):
     if (i % k != 0):
